70de5aeda8044971c4fa8f0c2fe86e3c699b4e17
Matheus Dal Mago <matheus.dalmago@chipinside.com.br>
2014-05-13 13:04:56 -0300

    added user interaction

--- fluidos.py
+++ fluidos.py
@@ -13,19 +13,55 @@
 # Read the density of two fluids and plot the graph height X pressure
 
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
 import numpy as np
 
-x = np.arange(0,40,0.01)
+x = np.arange(0,20,0.01)
 
 density1 = int(input("Densidade do primeiro fluido [kg/m³]: "))
 density2 = int(input("Densidade do segundo fluido [kg/m³]: "))
 
-plt.grid(True)
-plt.xlabel('Height [m]')
-plt.ylabel('Pressure [Pa]')
+class Type:
+    def __init__(self):
+        self.unityIndex = 0
 
-plt.plot (x, x*density1*9.8, label='fluido 1', linewidth=2)
-plt.plot (x, x*density2*9.8, label='fluido 2', linewidth=2)
+    def change(self, event):
+        if self.unityIndex == 0:
+            self.unityIndex = 1
+            self.pascal()
+        else:
+            self.unityIndex = 0
+            self.atm()
 
-plt.legend()
+    def pascal(self):
+        ax1.cla()
+        ax1.plot (x, x*density1*9.8*10**(-3), label='fluido 1', linewidth=2)
+        ax1.plot (x, x*density2*9.8*10**(-3), label='fluido 2', linewidth=2)
+
+        ax1.grid(True)
+        ax1.legend()
+        ax1.set_xlabel('Height [m]')
+        ax1.set_ylabel('Pressure [kPa]')
+
+    def atm(self):
+        ax1.cla()
+        ax1.plot (x, x*density1*9.8*9.87*10**(-6),\
+                label='fluido 1', linewidth=2)
+        ax1.plot (x, x*density2*9.8*9.87*10**(-6), \
+                label='fluido 2', linewidth=2)
+
+        ax1.grid(True)
+        ax1.legend()
+        ax1.set_xlabel('Height [m]')
+        ax1.set_ylabel('Pressure [atm]')
+
+fig = plt.figure()
+ax1=fig.add_subplot(111)
+plt.ion()
+
+button = Button(ax1, 'click')
+unity = Type()
+button.on_clicked(unity.change)
+
+
 plt.show()
